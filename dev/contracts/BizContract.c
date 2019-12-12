d73e330d0863bf21abfd55132f54dbea73c74da9
Marveliu <897920245@qq.com>
2019-12-12 22:57:27 +0800

    update:contract

---
+++ dev/contracts/BizContract.c
@@ -0,0 +1,13 @@
+#include "vntlib.h"
+
+constructor BizContract()
+{
+}
+
+//声明Report
+// CALL void ReportData_Transaction(string name);
+
+UNMUTABLE
+void testcall(){
+    Report("Report Supervisor Success !");
+}
---
+++ dev/contracts/RemoteCall.c
@@ -0,0 +1,11 @@
+#include "vntlib.h"
+
+constructor RemoteCall()
+{
+}
+
+//声明CALL
+CALL void SetNickName(CallParams params, string name);
+CALL string SayHello(CallParams params);
+
+
